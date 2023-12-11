10307b193fa4c8a3cba6338b02823aab812c159b
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2023-12-10 22:24:28 -0500

    added top text tracery

--- src/commands/utility/meme.js
+++ src/commands/utility/meme.js
@@ -3,11 +3,14 @@ const { generateMeme } = require('../../helpers/memeGenerator');
 const tracery = require("tracery-grammar");
 
 const topTextTracery = tracery.createGrammar({
-
+	'origin': ["#lines#"],
+	'lines':["#start#"],
+	'start':["POV:", "My face when", "This is me when", "When someone asks me", "That face when", "That moment when",
+			 "One does not simply", "How I feel when", "TOP TEXT", "Me when", "The moment when", "That feeling when",
+	         "When you don't study for a test and", "When the"]
 });
 
 topTextTracery.addModifiers(tracery.baseEngModifiers);
-
 
 const bottomTextTracery = tracery.createGrammar({
 
