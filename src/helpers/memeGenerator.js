6c7edf28e056765cc1af48304bf0d65d5d411d47
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-12-11 12:24:09 -0500

    Add documentation

--- src/helpers/memeGenerator.js
+++ src/helpers/memeGenerator.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+/**
+ * Fetches the JSON of the top 100 memes from imgflip
+ * @returns JSON containing the top 100 memes
+ */
 async function getMemes() {
 	const response = await fetch('https://api.imgflip.com/get_memes', {
 		method: 'GET',
@@ -8,6 +12,12 @@ async function getMemes() {
 	return await response.json();
 }
 
+/**
+ * Randomly selects 1 of the top 100 memes and adds tracery grammars to it
+ * @param {*} topText What the top text should display
+ * @param {*} bottomText What the bottom text should display
+ * @returns URL to the meme
+ */
 async function generateMeme(topText, bottomText) {
 	const memesArray = await getMemes();
 
@@ -18,7 +28,9 @@ async function generateMeme(topText, bottomText) {
 	const response = await fetch(`https://api.imgflip.com/caption_image?username=${process.env.IMGFLIP_UN}&password=${process.env.IMGFLIP_PW}&template_id=${memeId}&text0=${topText}&text1=${bottomText}`, {
 		method: 'GET',
 	});
+
 	const json = await response.json();
+
 	return await json.data.url;
 }
 
