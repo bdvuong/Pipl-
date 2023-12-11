569bcf68e23607815cb332efa0bd394a90bc50ca
Brandon Vuong <57996169+bdvuong@users.noreply.github.com>
2023-12-11 12:24:20 -0500

    Add bottom text + grammar generation

--- src/commands/utility/meme.js
+++ src/commands/utility/meme.js
@@ -1,19 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { generateMeme } = require('../../helpers/memeGenerator');
-const tracery = require("tracery-grammar");
+const tracery = require('tracery-grammar');
 
 const topTextTracery = tracery.createGrammar({
-	'origin': ["#lines#"],
-	'lines':["#start#"],
-	'start':["POV:", "My face when", "This is me when", "When someone asks me", "That face when", "That moment when",
-			 "One does not simply", "How I feel when", "TOP TEXT", "Me when", "The moment when", "That feeling when",
-	         "When you don't study for a test and", "When the"]
+	'origin': ['#lines#'],
+	'lines':['#start#'],
+	'start':['POV:', 'My face when', 'This is me when', 'When someone asks me', 'That face when', 'That moment when',
+		'One does not simply', 'How I feel when', 'TOP TEXT', 'Me when', 'The moment when', 'That feeling when',
+		'When you don\'t study for a test and', 'When the', 'DAE know', 'Imagine', 'When you gotta lock in:'],
 });
 
 topTextTracery.addModifiers(tracery.baseEngModifiers);
 
 const bottomTextTracery = tracery.createGrammar({
-
+	'origin': ['#lines#'],
+	'lines': ['#start#'],
+	'start': ['Dad Lore', 'Bottom text', 'My dad works for #company#', 'In this economy?', 'Rent is due', 'Your new empire',
+		'Use the power of friendship to kill god!'],
+	'company': ['Roblox', 'Microsoft', 'Michaelsoft', 'Nintendo', 'Apple', 'Riot Games', 'Blizzard'],
 });
 
 bottomTextTracery.addModifiers(tracery.baseEngModifiers);
@@ -24,7 +28,7 @@ module.exports = {
 		.setName('meme')
 		.setDescription('Generate a random meme!'),
 	async execute(interaction) {
-		const response = await generateMeme();
+		const response = await generateMeme(topTextTracery.flatten('#origin#'), bottomTextTracery.flatten('#origin#'));
 
 		await interaction.reply(response);
 	},
